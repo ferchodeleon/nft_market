9742f208877956327c8d54ef36356d81ec49bffa
Fernando De Leon <gasnare@hotmail.com>
2022-03-14 18:05:26 -0500

    add details of page ntfs

--- lib/screens/ntf_screen.dart
+++ lib/screens/ntf_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ntf_marketplace/animations/fade_animation.dart';
+import 'package:ntf_marketplace/widgets/blur_container.dart';
 
 class NTFScreen extends StatelessWidget {
   const NTFScreen({Key? key, required this.image}) : super(key: key);
@@ -9,19 +11,143 @@ class NTFScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Stack(
-            children: [
-              Hero(
-                tag: image,
-                child: Image.asset(image),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                Hero(
+                  tag: image,
+                  child: Image.asset(image),
+                ),
+                Positioned(
+                  child: FadeAnimation(
+                    child: BlurContainer(
+                      child: Container(
+                        width: 160,
+                        height: 40,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white.withOpacity(0.1),
+                        ),
+                        child: Text(
+                          'Digital NFT Art',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(height: 30),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0),
+              child: FadeAnimation(
+                intervalStart: 0.3,
+                child: Text(
+                  'Monkey #271',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
-            ],
-          )
-        ],
+            ),
+            const SizedBox(height: 5),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0),
+              child: FadeAnimation(
+                intervalStart: 0.35,
+                child: Text(
+                  'Owned by Gennady',
+                  style: TextStyle(color: Colors.white70),
+                ),
+              ),
+            ),
+            const SizedBox(height: 50),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: FadeAnimation(
+                intervalStart: 0.4,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    _InfoTitle(title: '3d 5h 23m', subTitle: 'Remainig Time'),
+                    _InfoTitle(title: '16.7 ETH', subTitle: 'Highest Bid'),
+                  ],
+                ),
+              ),
+            ),
+            const Spacer(),
+            FadeAnimation(
+              intervalStart: 0.6,
+              child: Container(
+                width: double.infinity,
+                height: 50,
+                alignment: Alignment.center,
+                margin: const EdgeInsets.symmetric(horizontal: 8),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: const Color(0xff3000ff),
+                ),
+                child: const FadeAnimation(
+                  child: Text(
+                    'Place Bid',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 50),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class _InfoTitle extends StatelessWidget {
+  const _InfoTitle({
+    Key? key,
+    required this.title,
+    required this.subTitle,
+  }) : super(key: key);
+
+  final String title;
+  final String subTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 5),
+        Text(
+          subTitle,
+          style: const TextStyle(
+            color: Colors.white70,
+          ),
+        ),
+      ],
     );
   }
 }
