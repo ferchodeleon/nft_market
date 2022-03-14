f22cac0cc500f8fe6377262f8c31cb3676cff51f
Fernando De Leon <gasnare@hotmail.com>
2022-03-14 16:46:14 -0500

    Incluided of images profile

---
+++ lib/widgets/image_list_view.dart
@@ -0,0 +1,93 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:ntf_marketplace/screens/ntf_screen.dart';
+
+class ImageListView extends StatefulWidget {
+  ImageListView({Key? key, required this.startIndex, this.duration = 30})
+      : super(key: key);
+
+  final int startIndex;
+  final int duration;
+  @override
+  State<ImageListView> createState() => _ImageListViewState();
+}
+
+class _ImageListViewState extends State<ImageListView> {
+  late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    _scrollController.addListener(() {
+      if (_scrollController.position.atEdge) {
+        _autoScroll();
+      }
+    });
+
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
+      _autoScroll();
+    });
+    super.initState();
+  }
+
+  _autoScroll() {
+    final _currentScrollPosition = _scrollController.offset;
+    final _scrollEndPosition = _scrollController.position.maxScrollExtent;
+
+    scheduleMicrotask(() {
+      _scrollController.animateTo(
+        _currentScrollPosition == _scrollEndPosition ? 0 : _scrollEndPosition,
+        duration: Duration(seconds: widget.duration),
+        curve: Curves.linear,
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: 1.96 * pi,
+      child: SizedBox(
+        height: 130,
+        child: ListView.builder(
+          controller: _scrollController,
+          itemCount: 10,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (BuildContext context, int index) {
+            return _ImageTitle(
+                image: 'assets/ntfs/${widget.startIndex + index}.png');
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class _ImageTitle extends StatelessWidget {
+  _ImageTitle({Key? key, required this.image}) : super(key: key);
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => NTFScreen(),
+          ),
+        );
+      },
+      child: Hero(
+        tag: image,
+        child: Image.asset(
+          image,
+          width: 130,
+        ),
+      ),
+    );
+  }
+}
