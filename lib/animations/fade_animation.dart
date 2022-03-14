3096b06d4be48a9675e2577f90f96bda915dc162
Fernando De Leon <gasnare@hotmail.com>
2022-03-14 16:50:58 -0500

    Fade animation

---
+++ lib/animations/fade_animation.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FadeAnimation extends StatelessWidget {
+  const FadeAnimation({Key? key}) : super(key: key);
+
+  ///Default value 0
+  final double begin;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
